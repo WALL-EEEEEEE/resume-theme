868dc65043936b923ac4acca4b725d0a02bd8509
mr.bian <mr.bian@mrbians-MacBook-Pro.local>
2021-12-08 09:46:15 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import WorkExpirence from './components/WorkExpirence.vue'
+import vuetify from '@/plugins/vuetify'
+new Vue({
+  vuetify,
+}).$mount('#app')
+</script>
+
+<template>
+  <WorkExpirence></WorkExpirence>
+</template>
+
+<style>
+</style>
---
+++ src/components/WorkExpirence.vue
@@ -0,0 +1,42 @@
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+    setup() {
+        
+    },
+})
+</script>
+
+
+<template>
+  <div class="work">
+    <span class="category-header">
+      {{ .Site.Data.content.work.icon | safeHTML}}
+      <h2>{{ .Site.Data.content.work.title }}</h2>
+    </span>
+
+    <div class="timeline-container">
+      {{ range .Site.Data.content.work.items }}
+        <div class="timeline-item">
+          <span class="date">{{ .timeStart }} - {{ .timeEnd }}</span>
+          <h3>
+            {{ .position }} @
+            <a href="{{ .employerUrl | safeHTML }}" target="_blank" rel="noreferrer">{{ .employerName }}</a>
+          </h3>
+          <p>
+            {{ .text | safeHTML }}
+            {{ if .list.enable }}
+              <ul>
+                {{ range .list.items }}
+                   <li>{{ .text | safeHTML }}</li>
+                 {{ end }}
+              </ul>
+            {{ end }}
+          </p>
+        </div>
+      {{ end }}
+    </div>
+  </div>
+  <hr>
+</template>
